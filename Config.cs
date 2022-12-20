520917d840cfcaa601db1013ff78bbeb66c67bf2
Vasil Orakov <vaskoorakov17@gmail.com>
2022-12-20 18:49:55 +0200

    Add files via upload

---
+++ Config.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Accessing_the_EFC
+{
+    internal static class Config
+    {
+        public static string ConnectionString =
+            @"Server=DESKTOP-NDPNSQD;
+            Database=SoftUni;
+            Integrated Security = True";
+    }
+}
