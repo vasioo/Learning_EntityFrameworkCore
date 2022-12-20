2b02cc49a443ec61ad1ea156c5ff7c5c9b3796df
Vasil Orakov <vaskoorakov17@gmail.com>
2022-12-20 18:59:11 +0200

    Add files via upload

---
+++ Album.cs
@@ -0,0 +1,37 @@
+﻿using MusicHub.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
+namespace MusicHub.Data.Models
+{
+    public class Album
+    {
+        public Album()
+        {
+            this.Songs = new HashSet<Song>();
+        }
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(GlobalConstant.AlbumMaxLength)]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+        [NotMapped]
+        public decimal Price
+            => this.Songs.Sum(song => song.Price);
+
+        [ForeignKey(nameof(Producer))]
+        public int? ProducerId{ get; set; }
+        public virtual Producer Producer { get; set; }
+
+        public ICollection<Song> Songs { get; set; }
+
+    }
+}
---
+++ Producer.cs
@@ -0,0 +1,24 @@
+﻿using MusicHub.Common;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MusicHub.Data.Models
+{
+    public class Producer
+    {
+        public Producer()
+        {
+            this.Albums = new HashSet<Album>();
+        }
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(GlobalConstant.ProducerMaxLength)]
+        public string Name { get; set; }
+
+        public string Pseudonym { get; set; }
+        public string PhoneNumber { get; set; }
+        public ICollection<Album> Albums { get; set; }
+    }
+}
