3054e4d12502e4da452f78431ff2bf418fceaccc
Vasil Orakov <vaskoorakov17@gmail.com>
2022-12-20 18:45:51 +0200

    Add files via upload

---
+++ Config.cs
@@ -0,0 +1,10 @@
+﻿namespace ADO.NET_exercises
+{
+    class Config
+    {
+        public static string ConnectionString =
+            @"Server=DESKTOP-NDPNSQD;
+            Database=MinionsDB;
+            Integrated Security = True";
+    }
+}
