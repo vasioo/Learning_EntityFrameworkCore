e43cc2ccd0434ad06057afde2deeeee6bf445e84
Vasil <vaskoorakov17@gmail.com>
2022-12-20 18:55:37 +0200

    EFC new files

---
+++ JSON/Other/Lichnainfo.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+public class Rootobject
+{
+    [JsonProperty("name")]
+    public string name { get; set; }
+    public int age { get; set; }
+    public string[] read { get; set; }
+}
