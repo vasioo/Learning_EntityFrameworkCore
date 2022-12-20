72ce145cef0386c65c9b895be46ab5676f0a3e4f
Vasil Orakov <vaskoorakov17@gmail.com>
2022-12-20 19:53:58 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,29 +1,12 @@
-﻿using LanguageIntegratedQueryInEFC.Models;
-using System;
-using System.Linq;
-using Microsoft.EntityFrameworkCore;
+﻿using System;
 
-namespace LanguageIntegratedQueryInEFC
+namespace LINQ_Operations
 {
     class Program
     {
         static void Main(string[] args)
         {
-            using (var context = new SoftUniContext())
-            {
-                var employees = context.Employees
-                    .Where(e => e.DepartmentId == 6)
-                    .Include(e => e.Address)
-                    .ThenInclude(a => a.Town)
-                    .Select(e => new
-                    {
-                        FullName = $"{e.FirstName} {e.LastName}",
-                        JobTitle = e.JobTitle,
-                        Town = e.Address.Town.Name    
-                    })
-                    .GroupBy(e=>e.Town)
-                    .ToList();
-            }
+            
         }
     }
 }
