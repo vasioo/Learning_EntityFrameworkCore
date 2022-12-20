1ecf672f61a224e259456fc94c0e50e22a158f51
Vasil Orakov <vaskoorakov17@gmail.com>
2022-12-20 19:53:16 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using LanguageIntegratedQueryInEFC.Models;
+using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanguageIntegratedQueryInEFC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new SoftUniContext())
+            {
+                var employees = context.Employees
+                    .Where(e => e.DepartmentId == 6)
+                    .Include(e => e.Address)
+                    .ThenInclude(a => a.Town)
+                    .Select(e => new
+                    {
+                        FullName = $"{e.FirstName} {e.LastName}",
+                        JobTitle = e.JobTitle,
+                        Town = e.Address.Town.Name    
+                    })
+                    .GroupBy(e=>e.Town)
+                    .ToList();
+            }
+        }
+    }
+}
